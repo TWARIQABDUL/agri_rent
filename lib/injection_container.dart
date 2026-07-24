e6598d3d2fa99d21d79de89ca10acb7d6f1a4039
giseleMwizera <mamengisele@gmail.com>
2026-07-24 10:41:42 +0200

    chore(di): register FirebaseAuth, GoogleSignIn and auth graph in GetIt

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 import 'injection_container.config.dart';
 
@@ -16,4 +18,10 @@ void configureDependencies() => sl.init();
 abstract class FirebaseModule {
   @lazySingleton
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
+
+  @lazySingleton
+  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+
+  @lazySingleton
+  GoogleSignIn get googleSignIn => GoogleSignIn.instance;
 }
